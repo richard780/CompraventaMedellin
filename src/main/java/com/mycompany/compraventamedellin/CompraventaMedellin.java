09b8d1250d7fbab597480fd30ce7f78ffc411682
Ricahrd <richard520ruiz@gmail.com>
2025-01-23 21:48:58 -0500

    first commit

---
+++ src/main/java/com/mycompany/compraventamedellin/CompraventaMedellin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compraventamedellin;
+
+/**
+ *
+ * @author richa
+ */
+public class CompraventaMedellin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
